2b6028d3a86a44ec3a90a4bcdbfa03aa22e20578
Ariel Flesler <aflesler@gmail.com>
2013-04-06 19:36:54 -0300

    Exposed $.actions.parseAction() and $.actions.toActionString() Better comments on different possible arguments to $.actions Handler receives a callback as last arg that triggers an end event on button If handler returns false, there's no event or e.stopPropagation()

--- jquery.domActions.js
+++ jquery.domActions.js
@@ -4,53 +4,70 @@
  * Dual licensed under MIT and GPL.
  * https://github.com/flesler/jquery.domActions
  * @author Ariel Flesler
- * @version 1.0.0
+ * @version 0.1.0
  */
-
+ 
 ;(function( $ ){
 	
+	// Simply a shortcut for $('body').actions()
 	var $actions = $.actions = function(arg, arg2){
 		return $('body').actions(arg, arg2);
 	};
 
+	// Can be changed externally
 	$actions.attr = 'data-action';
-	$actions.separator = '/';
 
 	$.fn.actions = function(arg, arg2){
 		if (arg === null) {
-			// $.actions(null);
+			// (null) -> Turn off
 			return this.unbind('click', monitor);
 		}
 
 		if ($.isFunction(arg)){
-			// $.actions(function(action, args){ });
+			// (function(action, args)) -> Catch all handler
 			data(this, '_handler', arg);
 		}
 		if ($.isPlainObject(arg)) {
-			// $.actions({ a:function(){} });
+			// (Object<string, function()>) -> List of handlers
 			for (var action in arg) {
 				data(this, action, arg[action]);
 			}
 		}
 		if (typeof arg === 'string') {
-			// $.actions('a', null);
+			// (string, null) -> Clear action handler
 			if (arg2 === null) {
 				return this.removeData('actions.'+arg);
 			}
 			if ($.isFunction(arg2)) {
-				// $.actions('a', function(){ });
+				// (string, function()) -> Add action handler
 				data(this, arg, arg2);
 			} else {
-				// $.actions('a');
-				// $.actions('a', [1,2,3]);
+				// ("") -> Clear action from element
+				// (string) -> Set action, no arguments
+				// (string, Array) -> Set action with arguments
 				arg2 = $.makeArray(arg2);
-				arg2.unshift(arg);
-				return this.attr($actions.attr, arg2.join($actions.separator));
+				return this.attr($actions.attr, $actions.toActionString(arg, arg2));
 			}
 		}
 
-		// Setup, clear in case it was bound already
+		// () -> Listen for actions / Turn on (most fall here)
+		// Clear in case it was bound already
 		return this.unbind('click', monitor).bind('click', monitor);
+	};
+
+	// Takes the string in the HTML, returns an array of action+arguments
+	// You can replace with a better one if needed
+	$actions.parseAction = function(action){
+		return $.map(action.split('/'), function(arg){
+			// Convert to number when applicable
+			return isNaN(arg) ? arg : parseFloat(arg);
+		});
+	};
+
+	// Takes action+arguments, returns the string in the HTML
+	// You can replace with a better one if needed
+	$actions.toActionString = function(action, args){
+		return action + '/' + args.join('/');
 	};
 
 	function data(obj, key, value) {
@@ -58,24 +75,38 @@
 	}
 
 	function monitor(e) {
-		var value = $(e.target).closest('['+$actions.attr+']').attr($actions.attr);
+		var button = $(e.target).closest('['+$actions.attr+']');
+		var value = button.attr($actions.attr);
 		if (!value) return;
 
 		e.preventDefault();
-		e.stopPropagation();
 
-		var args = value.split($actions.separator);
+		var args = $actions.parseAction(value);
 		var action = args.shift();
+		args.push(function(){
+			button.trigger(action+'-end', arguments);
+		});
 
 		var handler = $.data(this, 'actions.'+action);
 		if (handler) {
-			return handler.apply(null, args);
+			if (handler.apply(null, args) !== false) {
+				e.stopPropagation();
+				button.trigger(action, args);
+			}
+			return;
 		}
 
 		handler = $.data(this, 'actions._handler');
 		if (handler) {
-			handler(action, args);
+			if (handler(action, args) !== false) {
+				e.stopPropagation();
+				button.trigger(action, args);
+			}
 		}
+
+		// TODOs:
+		// maybe event {action} can preventDefault the handler, or an {action}-pre
+		// $.actions('a', null) also removes data-action ?
 	}
 
 })( jQuery );
