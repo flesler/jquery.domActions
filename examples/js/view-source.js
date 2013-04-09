b4857301349bb12183e94522e4ea568b6400d04e
Ariel Flesler <aflesler@gmail.com>
2013-04-08 21:14:59 -0300

    Made examples compatible with htmlpreview.github.com

--- examples/js/view-source.js
+++ examples/js/view-source.js
@@ -1,3 +1,6 @@
 $(function(){
-	$('<pre />').text( $('body').html().replace(/<h1>.*<\/h1>\s*/, '') ).appendTo('body');
+  var code = $('body').html().replace(/<h1>.*<\/h1>\s*/, '');
+  // Make compatible with htmlpreview.github.com
+  code = code.replace(/(\/script\S+)(\s|.)*/, '$1');
+  $('<pre />').text( code ).appendTo('body');
 });
